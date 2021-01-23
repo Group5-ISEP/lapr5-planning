7c6c1bec0c8cf16fd96036218b19e693aea42f9e
zemiguel20 <migli.coutinho@gmail.com>
2021-01-23 22:08:46 +0000

    sprint c atualiza os vehicle duties no final

--- geracao.pl
+++ geracao.pl
@@ -14,9 +14,12 @@
 :- [selecao].
 
 gera_geracao(G,G,Pop):-!,
-	write('Geração '), write(G), write(':'), nl, write(Pop), nl.
+	%write('Geração '), write(G), write(':'), nl, write(Pop), nl.
+	Pop = [ MelhorInd*V | _ ],
+	write('Melhor: '), write(MelhorInd), write(' * '), write(V), nl,
+	atualizar_motoristas(MelhorInd).
 gera_geracao(N,G,Pop):-
-	write('Geração '), write(N), write(':'), nl, write(Pop), nl,
+	%write('Geração '), write(N), write(':'), nl, write(Pop), nl,
 	cruzamento(Pop,NovaPop),
 	mutacao(NovaPop,NovaPopMutada),
 	avalia_populacao(NovaPopMutada,NPopAv),
@@ -26,4 +29,48 @@ gera_geracao(N,G,Pop):-
 	gera_geracao(N1,G,SelecaoOrd).
 
 %-----------------------------------------------------------------------------------------------------------------------
+%-----------------------------------------------------------------------------------------------------------------------
+atualizar_motoristas(MelhorInd):-
+	atualizar_motoristas1(MelhorInd,0), !.
+
+atualizar_motoristas1([],_):-!.
+atualizar_motoristas1(MelhorInd,Index):-
+
+	% Busca o workblock em Index
+	vehicle_duty_id(VDId),
+	vehicleduty(VDId,ListaWorkBlocks),
+	nth0(Index,ListaWorkBlocks,Workblock),
+
+	% Encaixa workblock na lista do motorista
+	MelhorInd = [Motorista | Resto],
+	motorista(Motorista, WorkblocksMotorista),
+	adicionar_workblock(WorkblocksMotorista,Workblock,NovaLista),
+
+	retract(motorista(Motorista,_)),
+	asserta(motorista(Motorista,NovaLista)),
+
+	write('Motorista: '), write(Motorista), write(' : '), write(NovaLista), nl,
+	Index1 is Index + 1,
+	atualizar_motoristas1(Resto,Index1).
+
+
+
+
+adicionar_workblock([],NovoWorkBlock,NovaLista):-
+	append([NovoWorkBlock],[],NovaLista).
+adicionar_workblock(ListaWorkBlocks, NovoWorkBlock, NovaLista):-
+	ListaWorkBlocks = [ WorkBlock | Resto ],
+
+	workblock(NovoWorkBlock,_,StartTime1,_),
+	workblock(WorkBlock,_,StartTime2,_),
+
+	(
+		( StartTime1<StartTime2, append( [NovoWorkBlock], ListaWorkBlocks, NovaLista) )
+		;
+		( adicionar_workblock(Resto,NovoWorkBlock,NovoResto), append( [WorkBlock],NovoResto,NovaLista) )
+	),
+
+	!.
+
+
 %-----------------------------------------------------------------------------------------------------------------------
