c47ee959073a1127f920536c918aa5d88848b316
zemiguel20 <migli.coutinho@gmail.com>
2021-01-23 22:24:11 +0000

    sprint d atribuir gerar driver duties para cada vehicle duty

--- sprintd.pl
+++ sprintd.pl
@@ -1,6 +1,23 @@
 
 :-[sprintb].
 :-[sprintc].
+:-[gerar_motoristas_workblock].
+
+
+reset_motoristas():-
+    findall(
+        _,
+        (
+            retract(motorista(Id,_)),
+            asserta(motorista(Id,[]))
+        ),
+        _
+    ).
 
 atribuir_motoristas():-
-    gera_ligacoes.
+    gera_ligacoes,
+    gerar_n_motoristas_workblock(),
+
+    findall(_, (vehicleduty(Id,_), gera(Id)),_ ).
+
+
